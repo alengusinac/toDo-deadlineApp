0d1598044e1203859299790faa59237e2cb5ffa6
Alen Gusinac <alengusinacglobe@gmail.com>
2022-12-09 20:29:52 +0800

    added header eventlisteners and openContainer function

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,37 @@
 import './style/style.scss';
+
+/* **********************VARIABLES********************** */
+
+// HEADER BUTTONS
+const sortBtn = document.querySelector('#sorting-btn');
+const categoriesBtn = document.querySelector('#categories-btn');
+const addBtn = document.querySelector('#add-btn');
+
+// HEADER CONTAINERS
+const sortContainer = document.querySelector('.sort-container');
+const addContainer = document.querySelector('.add-item-container');
+
+/* **********************FUNCTIONS********************** */
+
+function openContainer(e): void {
+  const targetID = e.currentTarget.id;
+
+  if (targetID === 'sorting-btn') {
+    sortContainer?.classList.toggle('open');
+    if (addContainer?.classList.contains('open')) {
+      addContainer?.classList.remove('open');
+    }
+  } else if (targetID === 'add-btn') {
+    addContainer?.classList.toggle('open');
+    if (sortContainer?.classList.contains('open')) {
+      sortContainer?.classList.remove('open');
+    }
+  }
+}
+
+/* **********************LOGIC********************** */
+
+// HEADER EVENTLISTENERS
+sortBtn?.addEventListener('click', openContainer);
+categoriesBtn?.addEventListener('click', openContainer);
+addBtn?.addEventListener('click', openContainer);
